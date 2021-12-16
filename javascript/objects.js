d02da9cab60bbaa6f80b9226129641c7c356f35f
Rian Dias de Oliveira <riandiasdeoliveira2001@gmail.com>
2021-12-16 16:45:47 -0300

    Delete old files

--- javascript/objects.js
+++
@@ -1,9 +0,0 @@
-// Declaração de um objeto, semelhante aos arrays.
-
-const userName = {
-    first: 'Rian ',
-    middle: 'Dias ',
-    last: 'de Oliveira'
-};
-
-console.log(userName.first + userName.middle + userName.last);
