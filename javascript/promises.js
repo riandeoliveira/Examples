1c19409f5b5f45bc5af93fd5831dde6362674bbc
Rian Dias de Oliveira <riandiasdeoliveira2001@gmail.com>
2021-12-17 14:28:49 -0300

    Add promises example

--- javascript/promises.js
+++ javascript/promises.js
@@ -1,1 +1,26 @@
-// Pesquisar sobre...
+// Promises
+
+
+
+// Código SÍNCRONO
+
+const sumNumbers = () => {
+    let result = 1 + 1;
+    result == 2 ? sucessCallback() : errorCallback();
+}
+
+const sucessCallback = () => console.log("Yeah! Number 2");
+
+const errorCallback = () => console.log("Oops! Something wrong.");
+
+sumNumbers();
+
+// Código ASSÍNCRONO
+
+const promise = new Promise((resolve, reject) => {
+    let result = 1 + 1;
+    result == 2 ? resolve("Sucess") : reject("Failed");
+});
+
+promise.then(message => console.log("This is in the then " + message))
+    .catch(err => console.log("This is in the catch " + err));
