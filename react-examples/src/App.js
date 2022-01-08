76232a33258fd2836b9d6fd6decbbafe0104c81b
Rian Oliveira <riandiasdeoliveira2001@gmail.com>
2022-01-08 02:36:29 -0300

    Update App

--- react-examples/src/App.js
+++ react-examples/src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
+// Importações de arquivos com códigos que serão usados neste componente.
 import './App.css';
+import Components from './examples/Components';
+import Props from './examples/props/Props';
+import Example from './examples/modules/Example';
+import Fragments from './examples/Fragments';
+import Types from './examples/props/Types';
+import Events from './examples/Events';
+import States from './examples/States';
 
-function App() {
+const App = () => {
+  const welcome = 'Olá, Mundo!';
+  const phrase = 'Estou aprendendo React.';
+
+
+  {/* Renderização dos componentes. */ }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>{welcome}</h1>
+        <p>{phrase}</p>
       </header>
+      <main>
+        {/* Exemplo de Componentes. */}
+        <Components />
+
+        {/* Exemplos de Props. */}
+        <Props fullName='Jonas Roberto' age='17' sex='M' />
+        <Props fullName='Paulo Silva' age='24' sex='M' />
+        <Props fullName='Angélica Gonçalvez' age='19' sex='F' />
+
+        {/* Exemplos de Módulos CSS. */}
+        <Example />
+
+        {/* Exemplo de Fragmentos. */}
+        <Fragments />
+
+        {/* Exemplos de PropTypes. */}
+        <Types brand='Ferrari' releaseYear={1985} />
+        <Types brand='Fiat' releaseYear={1964} />
+        <Types brand='Renault' />
+
+        {/* Exemplo de Eventos. */}
+        <Events btn={1} />
+        <Events btn={2} />
+
+        {/* Exemplos de Estados. */}
+        <States />
+      </main>
     </div>
   );
 }
