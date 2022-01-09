6d6caffb13375c2ec308a8bf345da48cdf082b5e
Rian Oliveira <riandiasdeoliveira2001@gmail.com>
2022-01-09 12:29:30 -0300

    Create info about sequelize module

---
+++ node.js/sequelize.js
@@ -0,0 +1,4 @@
+// Sequelize é um módulo que permite trabalhar com bancos de dados relacionais, como o MySQL, através do Node. Para usá-lo é preciso fazer algumas instalações utilizando os comandos:
+
+// npm install --save sequelize
+// npm install --save mysql2
