28a59da8fb00feb9def8e814bbf940a4963b7e75
riandeoliveira <riandiasdeoliveira2001@gmail.com>
2022-09-18 00:44:03 -0300

    refactor(javascript): remove old examples

--- javascript/forEach.js
+++
@@ -1,5 +0,0 @@
-// O forEach pega cada elemento de um array e executa algo.
-
-const example = ['123', 'abc', 'Rian', "AS"];
-
-example.forEach(() => console.log(example));
--- javascript/rest.js
+++
@@ -1,7 +0,0 @@
-// Operador Rest.
-
-// Utilizado quando não se sabe quantos argumentos a função receberá.
-
-const addItem = (...items) => console.log(items);
-
-addItem("Rian", 20, true);
