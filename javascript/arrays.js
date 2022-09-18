24c3bf43bca3fa670f852f95df46a4bf863c304d
riandeoliveira <riandiasdeoliveira2001@gmail.com>
2022-09-18 00:24:57 -0300

    feat(javascript) update array examples

--- javascript/arrays.js
+++ javascript/arrays.js
@@ -1,64 +1,25 @@
-// Métodos de Arrays.
+// Array Methods
 
+const names = ["Rick", "Jane", "Michelle", "Richard"];
+const numbers = [45, 4, 9, 16, 25];
 
-
-const methods = [];
-
-const example1 = ["Rian", 20, true, 15.12];
-const example2 = ["Giullianno", "Márcia", "Jamile", "Gustavo"];
-const example3 = [45, 4, 9, 16, 25];
-
-// Transforma todo o array em uma string.
-methods[0] = example1.toString();
-
-// Transforma todo o array em uma string e separa os elementos pelo caractere desejado.
-methods[1] = example1.join(" – ");
-
-// Retorna o indíce (posição) do elemento desejado.
-methods[2] = example1.indexOf(15.12);
-
-// Remove o último item do array, ALTERANDO ele.
-methods[3] = example1.pop();
-
-// Remove o primeiro item do array, ALTERANDO ele.
-methods[4] = example1.shift();
-
-// Adiciona um item no final do array, ALTERANDO ele.
-methods[5] = example1.push(15.12);
-
-// Adiciona um item no início do array, ALTERANDO ele.
-methods[6] = example1.unshift("Rian");
-
-// Recebe um indíce e a quantidade de itens a serem removidos e os deleta do array, ALTERANDO ele.
-methods[7] = example1.splice(1, 2);
-
-// Concatena um array com outro array.
-methods[8] = example1.concat(example2);
-
-// Ordena o array em ordem alfabética, ALTERANDO ele.
-methods[9] = example2.sort();
-
-// Inverte a ordem do array.
-methods[10] = example1.reverse();
-
-// Mapeia o array, executando uma função para cada um dos itens, gerando um novo array com os resultados.
-methods[11] = example3.map(item => item * 2);
-
-// Filtra um array, adicionando o elemento verificado em um novo array se ele for true.
-methods[12] = example3.filter(item => item < 20 ? true : false);
-
-// Retorna true se a condição desejada for verdadeira para todos os itens do array, senão retorna false.
-methods[13] = example3.every(item => item > 20 ? true : false);
-
-// Retorna true se a condição desejada for verdadeira para pelo menos um item do array, senão retorna false.
-methods[14] = example3.some(item => item > 20 ? true : false);
-
-// Procura um item no array e o retorna se encontrar.
-methods[15] = example3.find(item => item === 16 ? true : false);
-
-// Procura um item no array e retorna o indíce dele se o encontrar.
-methods[16] = example3.findIndex(item => item === 16 ? true : false);
-
-
-
-methods.map((item, i) => console.log(`${typeof item} – ${i}: ${item}`));
+names.includes("Rick"); // true
+names.indexOf("Michelle"); // 2
+names.join(" – "); // Rick – Jane – Michelle – Richard
+names.lastIndexOf("Jane"); // 1
+names.pop(); // ["Rick", "Jane", "Michelle"]
+names.push("Nancy"); // ["Rick", "Jane", "Michelle", "Richard", "Nancy"]
+names.shift(); // ["Jane", "Michelle", "Richard"]
+names.slice(1, 3); // ["Jane", "Michelle"]
+names.sort(); // ["Jane", "Michelle", "Richard", "Rick"]
+names.splice(0, 2); // ["Rick", "Jane"]
+names.toString(); // Rick,Jane,Michelle,Richard
+names.unshift("Joseph"); // ["Joseph", "Rick", "Jane", "Michelle", "Richard"]
+numbers.every((item) => item > 20); // false
+numbers.filter((item) => item < 20); // [ 16, 9, 4 ]
+numbers.find((item) => item === 16); // 16
+numbers.findIndex((item) => item === 16); // 3
+numbers.map((item) => item * 2); // [ 50, 32, 18, 8, 90 ]
+numbers.reduce((accumulator, item) => accumulator + item, 0); // 99
+numbers.reverse(); // ["Richard", "Michelle", "Jane", "Rick"]
+numbers.some((item) => item > 20); // true
