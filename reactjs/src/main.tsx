5ea05f29edd9e70e81e6cc51d09e266b6d330ad4
riandeoliveira <riandiasdeoliveira2001@gmail.com>
2022-09-19 11:39:29 -0300

    refactor(reactjs): update reacjs example

--- reactjs/src/main.tsx
+++ reactjs/src/main.tsx
@@ -1,11 +1,10 @@
-import ReactDOM from "react-dom/client";
-import React from "react";
-import { GlobalStyle } from "./index";
-import { App } from "./App";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <GlobalStyle />
     <App />
   </React.StrictMode>
-);
+)
