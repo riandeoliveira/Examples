363e4cfe35ad16ee9215c7d1646923b351484507
riandeoliveira <riandiasdeoliveira2001@gmail.com>
2022-10-05 02:05:35 -0300

    feat(python): create new examples

---
+++ python/loops.py
@@ -0,0 +1,21 @@
+count = 1
+
+while count <= 3:
+    print(count)  # 1, 2, 3
+
+    count += 1
+
+for i in range(5):
+    print(i)  # 0, 1, 2, 3, 4
+
+for j in range(1, 9, 2):
+    print(j)  # 1, 3, 5, 7
+
+for k in 'John':
+    print(k)  # J, o, h, n
+
+for l in ["apple", "banana", "cherry"]:
+    print(l)  # apple, banana, cherry
+
+for index, element in enumerate(['John', 20, True]):
+    print(index, element)  # 0 John, 1 20, 2 True
---
+++ python/tuples.py
@@ -0,0 +1,4 @@
+my_tuple = ('John', 20, True)
+
+my_tuple.count('John')  # 1
+my_tuple.index(True)  # 2
