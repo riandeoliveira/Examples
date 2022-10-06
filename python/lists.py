c9ce0f1032cbe9e5a330e7e10a0a9fd336c7e04a
riandeoliveira <riandiasdeoliveira2001@gmail.com>
2022-10-06 14:35:48 -0300

    refactor(python): update lists example

--- python/lists.py
+++ python/lists.py
@@ -1,7 +1,5 @@
 my_list = ["John", 20, True]
 
-len(my_list)  # 3
-
 my_list.append("Doe")  # ["John", 20, True, "Doe"]
 my_list.extend([1, 2, 3])  # ["John", 20, True, 1, 2, 3]
 my_list.pop(1)  # ["John", True]
