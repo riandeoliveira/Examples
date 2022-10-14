9bf7198bdf3616a3b1969fc630eebaa09c3f6f32
riandeoliveira <riandiasdeoliveira2001@gmail.com>
2022-10-14 16:21:01 -0300

    feat(shell): create cmd commands as examples

---
+++ shell/windows.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# checks the file system integrity of a hard drive
+chkdsk
+
+# displays and manages the IP address assigned to the machine
+ipconfig
+
+# displays the contents of various network-related data structures for active connections
+netstat
+
+# used to test connectivity between two machines
+ping
+
+# checks the integrity of protected system files and fixes errors automatically
+sfc
+
+# turns off the computer after a certain time in seconds
+shutdown /s /t 30
+
+# displays detailed configuration information about a computer and its operating system
+systeminfo
+
+# kill a process or task from the command line
+taskkill /im "program.exe"
+
+# displays a list of currently running processes on the local computer or on a remote computer
+tasklist
+
+# prints the version of the Windows operating system running on the computer
+ver
+
+# displays the version of Windows that is running
+winver
