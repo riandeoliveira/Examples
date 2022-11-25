596eb4e800f9ff5f55080babce8d89a3954f7835
Rian Oliveira <riandiasdeoliveira2001@gmail.com>
2022-11-25 10:51:16 -0300

    feat(php): create dates examples

---
+++ php/dates.php
@@ -0,0 +1,8 @@
+<?php
+
+date("F j, Y, g:i a"); // November 25, 2022, 2:45 pm
+date("m.d.y"); // 11.25.22
+date("j, n, Y"); // 25, 11, 2022
+date("Ymd"); // 20221125
+date("H:i:s"); // 14:48:20
+date("Y-m-d H:i:s"); // 2022-11-25 14:48:35
