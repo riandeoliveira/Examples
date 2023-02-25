31598ba2149e62e6c71ba9ac88dd7d11cd1d11b7
Rian Oliveira <riandiasdeoliveira2001@gmail.com>
2023-02-25 15:48:39 -0300

    feat(c#): create C# examples

---
+++ c#/DataTypes.cs
@@ -0,0 +1,50 @@
+class DataTypes
+{
+    // System.Boolean
+    bool single = true; // true | false
+
+    // Unsigned 8-bit integer (System.Byte)
+    byte userAge = 21; // 0 to 255
+
+    // System.Char
+    char sex = 'M'; // single character
+
+    // System.Decimal
+    decimal iDontKnowWhatIsThis = 2334.64m; // 28-29 digits
+
+    // System.double
+    double userHeight = 1.85; // ~15-17 digits
+
+    // System.Single
+    float distance = 93.5f; // ~6-9 digits
+
+    // Signed 32-bit integer (System.Int32)
+    int counter = -768254387; // -2,147,483,648 to 2,147,483,647
+
+    // Signed 64-bit integer (System.Int64)
+    long universeAge = 13780000000; // -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
+
+    // Signed 32-bit or 64-bit integer (System.IntPtr)
+    nint negativeRandom = -428; // Depends on platform (computed at runtime)
+
+    // Unsigned 32-bit or 64-bit integer (System.UIntPtr)
+    nuint positiveRandom = 762; // Depends on platform (computed at runtime)
+
+    // Signed 8-bit integer (System.SByte)
+    sbyte temperature = -12; // -128 to 127
+
+    // Signed 16-bit integer (System.Int16)
+    short chanceToStopLearning = 0; // -32,768 to 32,767
+
+    // System.String
+    string fullName = "John Doe"; // characters set
+
+    // Unsigned 32-bit integer (System.UInt32)
+    uint earthLifeAge = 3894967295; // 0 to 4,294,967,295
+
+    // Unsigned 64-bit integer (System.UInt64)
+    ulong noMansSkyPlanets = 18446744073709551615; // 0 to 18,446,744,073,709,551,615
+
+    // Unsigned 16-bit integer (System.UInt16)
+    ushort civilizationAge = 6000; // 0 to 65,535
+}
